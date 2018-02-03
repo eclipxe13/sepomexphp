b72d58bc7714322218cba915f86dee65d0b02cae
Carlos C Soto <eclipxe13@gmail.com>
2018-02-02 22:33:09 -0600

    Make phpstan happy when working on on possibly null value

--- tests/Cases/ZipCodeDataTest.php
+++ tests/Cases/ZipCodeDataTest.php
@@ -25,6 +25,10 @@ class ZipCodeDataTest extends \PHPUnit_Framework_TestCase
     {
         $sepomex = new SepomexPhp(new Gateway($this->getPDO()));
         $zipcode = $sepomex->getZipCodeData(86000);
+        $this->assertNotNull($zipcode);
+        if (null === $zipcode) {
+            return;
+        }
         $this->assertInstanceOf(ZipCodeData::class, $zipcode, 'The zip code was not found');
         $this->assertSame(86000, $zipcode->zipcode, 'The zipcode property does not match');
         $this->assertCount(3, $zipcode->locations, 'Not all locations were found');
