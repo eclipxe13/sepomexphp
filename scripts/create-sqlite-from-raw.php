6dc9dcd0133a251faf1a63d32a8c23e3028df9bb
Carlos C Soto <eclipxe13@gmail.com>
2018-03-09 15:10:06 -0600

    exit with status, use import instead of all steps

--- scripts/create-sqlite-from-raw.php
+++ scripts/create-sqlite-from-raw.php
@@ -7,38 +7,36 @@ declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // escape the global scope
-call_user_func(function () {
-    $path = dirname(__DIR__) . '/assets';
-    // create the path if this soes not exists
-    if (! is_dir($path)) {
-        mkdir($path, 0755, true);
+exit(call_user_func(function () {
+    try {
+        $path = dirname(__DIR__) . '/assets';
+        // create the path if this soes not exists
+        if (! is_dir($path)) {
+            mkdir($path, 0755, true);
+        }
+        // files
+        $dbfile = $path . '/sepomex.db';
+        $rawfile = $path . '/sepomex.txt';
+
+        // raw file
+        if (! file_exists($rawfile)) {
+            $sourceurl = 'http://www.correosdemexico.gob.mx/datosabiertos/cp/cpdescarga.txt';
+            echo "File $rawfile does not exists, will be downloaded from $sourceurl\n";
+            copy($sourceurl, $rawfile);
+        }
+
+        // create the pdo object
+        $pdo = new \PDO('sqlite:' . $dbfile, null, null, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+        // create the importer class
+        $importer = new \SepomexPhp\Importer\PdoImporter($pdo);
+
+        // follow this steps
+        $importer->createStruct();
+        $importer->import($rawfile, null);
+        return 0;
+    } catch (\Throwable $exception) {
+        file_put_contents('php://stderr', $exception->getMessage(), FILE_APPEND);
+        return 1;
     }
-    // files
-    $dbfile = $path . '/sepomex.db';
-    $rawfile = $path . '/sepomex.txt';
-
-    // raw file
-    if (! file_exists($rawfile)) {
-        $sourceurl = 'http://www.correosdemexico.gob.mx/datosabiertos/cp/cpdescarga.txt';
-        echo "File $rawfile does not exists, will be downloaded from $sourceurl\n";
-        copy($sourceurl, $rawfile);
-    }
-
-    // create the pdo object
-    $pdo = new \PDO('sqlite:' . $dbfile, null, null, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-
-    // create the importer class
-    $importer = new \SepomexPhp\Importer\PdoImporter($pdo);
-
-    // follow this steps
-    $importer->createStruct();
-    $importer->importRawTxt($rawfile);
-    $importer->populateStates();
-    $importer->populateDistricts();
-    $importer->populateCities();
-    $importer->populateZipCodes();
-    $importer->populateLocationTypes();
-    $importer->populateLocations();
-    $importer->populateLocationZipCodes();
-    $importer->clearRawTable();
-});
+}));
--- scripts/zipcode-info.php
+++ scripts/zipcode-info.php
@@ -1,55 +1,63 @@
 <?php
 
 declare(strict_types=1);
+
 /**
  * This script is a command line tool to get information about a zipcode
  * It uses the database on assets/sepomex.db
  * This database is not distributable but you can create it with create-sqlite-from-raw.php
- * Usage: zipcode-infp.php zipcode
+ *
+ * Usage: zipcode-info.php zipcode
  */
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // escape the global scope
-call_user_func(function ($argv) {
+exit(call_user_func(function (array $argv) {
     // exit if no arguments
     if (2 !== count($argv)) {
         echo 'Usage: ', $argv[0], " zipcode\n";
-        return;
+        return 1;
     }
 
-    // set the database location
-    $dbfile = __DIR__ . '/../assets/sepomex.db';
-    // create the PDO Object
-    $pdo = new PDO('sqlite:' . $dbfile, null, null, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-    // create the gateway
-    $gateway = new SepomexPhp\PdoGateway\Gateway($pdo);
-    // create the SepomexPhp Object
-    $sepomex = new \SepomexPhp\SepomexPhp($gateway);
+    try {
+        // set the database location
+        $dbfile = __DIR__ . '/../assets/sepomex.db';
+        // create the PDO Object
+        $pdo = new PDO('sqlite:' . $dbfile, null, null, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        // create the gateway
+        $gateway = new SepomexPhp\PdoGateway\Gateway($pdo);
+        // create the SepomexPhp Object
+        $sepomex = new \SepomexPhp\SepomexPhp($gateway);
 
-    // query a zip code
-    $zipcode = $sepomex->getZipCodeData($argv[1]);
-    if (null === $zipcode) {
-        echo 'Not found: ', $argv[1], "\n";
-        return;
-    }
-    $locations = $zipcode->locations();
-    $cities = $locations->cities();
-    $citiesCount = $cities->count();
-
-    // display information
-    echo '      ZipCode: ', $zipcode->format(), "\n";
-    echo '     District: ', $zipcode->district()->name(), "\n";
-    echo '        State: ', $zipcode->state()->name(), "\n";
-    if ($citiesCount > 1) {
-        echo '       Cities: ', $citiesCount, "\n";
-        foreach ($cities as $city) {
-            echo '               ', $city->name(), "\n";
+        // query a zip code
+        $zipcode = $sepomex->getZipCodeData($argv[1]);
+        if (null === $zipcode) {
+            echo 'Not found: ', $argv[1], "\n";
+            return 1;
         }
-    } else {
-        echo '         City: ', ($citiesCount > 0) ? $cities->byIndex(0)->name() : '(Ninguna)', "\n";
+        $locations = $zipcode->locations();
+        $cities = $locations->cities();
+        $citiesCount = $cities->count();
+
+        // display information
+        echo '      ZipCode: ', $zipcode->format(), "\n";
+        echo '     District: ', $zipcode->district()->name(), "\n";
+        echo '        State: ', $zipcode->state()->name(), "\n";
+        if ($citiesCount > 1) {
+            echo '       Cities: ', $citiesCount, "\n";
+            foreach ($cities as $city) {
+                echo '               ', $city->name(), "\n";
+            }
+        } else {
+            echo '         City: ', ($citiesCount > 0) ? $cities->byIndex(0)->name() : '(Ninguna)', "\n";
+        }
+        echo '    Locations: ', $locations->count() ? : '(Ninguna)', "\n";
+        foreach ($locations as $location) {
+            echo '               ', $location->getFullName(), "\n";
+        }
+        return 0;
+    } catch (\Throwable $exception) {
+        file_put_contents('php://stderr', $exception->getMessage(), FILE_APPEND);
+        return 1;
     }
-    echo '    Locations: ', $locations->count() ? : '(Ninguna)', "\n";
-    foreach ($locations as $location) {
-        echo '               ', $location->getFullName(), "\n";
-    }
-}, $argv);
+}, $argv));
