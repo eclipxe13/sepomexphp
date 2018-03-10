9b40fbc6c1d3c62bb6be81d239e3797b57b43e79
Carlos C Soto <eclipxe13@gmail.com>
2018-03-09 19:54:37 -0600

    State must not have locations property

--- src/SepomexPhp/State.php
+++ src/SepomexPhp/State.php
@@ -5,25 +5,21 @@ declare(strict_types=1);
 namespace SepomexPhp;
 
 use SepomexPhp\Traits\PropertyIdIntegerTrait;
-use SepomexPhp\Traits\PropertyLocationsTrait;
 use SepomexPhp\Traits\PropertyNameStringTrait;
 
 class State
 {
     use PropertyIdIntegerTrait;
     use PropertyNameStringTrait;
-    use PropertyLocationsTrait;
 
     /**
      * @param int $id
      * @param string $name
-     * @param Location[] $locations
      */
-    public function __construct(int $id, string $name, array $locations = [])
+    public function __construct(int $id, string $name)
     {
         $this->setId($id);
         $this->setName($name);
-        $this->setLocations(...$locations);
     }
 
     public function asArray(): array
@@ -31,7 +27,6 @@ class State
         return [
             'id' => $this->id(),
             'name' => $this->name(),
-            'locations' => $this->locations()->asArray(),
         ];
     }
 }
