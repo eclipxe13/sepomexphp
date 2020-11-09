9ce70aa259281b7de403be682320c0bc3154fc75
Carlos C Soto <eclipxe13@gmail.com>
2020-11-08 22:52:19 -0600

    State property now is never optional, reduce PropertyStateTrait

--- src/Data/Traits/PropertyStateTrait.php
+++ src/Data/Traits/PropertyStateTrait.php
@@ -5,26 +5,17 @@ declare(strict_types=1);
 namespace Eclipxe\SepomexPhp\Data\Traits;
 
 use Eclipxe\SepomexPhp\Data\State;
-use LogicException;
 
 trait PropertyStateTrait
 {
-    private ?State $state = null;
+    private State $state;
 
     public function state(): State
     {
-        if (! $this->state instanceof State) {
-            throw new LogicException('Try to access the state when no exists');
-        }
         return $this->state;
     }
 
-    public function hasState(): bool
-    {
-        return ($this->state instanceof State);
-    }
-
-    protected function setState(State $state = null)
+    protected function setState(State $state)
     {
         $this->state = $state;
     }
